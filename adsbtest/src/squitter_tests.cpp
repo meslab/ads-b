1f836d415160db0d94c622602b717d2df61e01ce
anton.sidorov <anton@plasseylabs.com>
2013-12-07 21:11:44 +0000

    test suites added

--- adsbtest/src/squitter_tests.cpp
+++ adsbtest/src/squitter_tests.cpp
@@ -6,66 +6,76 @@
 namespace
 {
 
-TEST(get_in_test)
+SUITE(Squitter)
 {
-    std::string in = "ffffdebeaf";
-    squitter* a = new squitter(in);
-    CHECK_EQUAL(in,a->get_in());
+    TEST(get_in_test)
+    {
+        std::string in = "ffffdebeaf";
+        squitter* a = new squitter(in);
+        CHECK_EQUAL(in,a->get_in());
 
-    delete a;
+        delete a;
+    }
+
+    TEST(get_bin_test_1)
+    {
+        std::string in = "f";
+        squitter* a = new squitter(in);
+        CHECK_EQUAL(15,(a->get_dec(0)));
+
+        delete a;
+    }
+
+    TEST(get_bin_test_2)
+    {
+        std::string in = "ff";
+        squitter* a = new squitter(in);
+        CHECK_EQUAL(255,(a->get_dec(0)*16 + a->get_dec(1)));
+
+        delete a;
+    }
 }
 
-TEST(hex_test_1)
+SUITE(hex2dec)
 {
-        int t{};
+    TEST(hex_test_1)
+    {
+        int t {};
         char c = '1';
         hex2dec(t,c);
         CHECK_EQUAL(1,t);
-}
+    }
 
 
-TEST(hex_test_over)
-{
+    TEST(hex_test_over)
+    {
         int t = 0;
         char c = 'g';
         CHECK_THROW(hex2dec(t,c), exception);
+    }
 }
 
-TEST(get_bin_test_1)
+SUITE(Squitter_short)
 {
-    std::string in = "f";
-    squitter* a = new squitter(in);
-    CHECK_EQUAL(15,(a->get_dec(0)));
+    TEST(get_bin_test_3)
+    {
+        std::string in = "ff";
+        squitter_short* a = new squitter_short(in);
+        CHECK_EQUAL(255,(a->get_dec(0)*16 + a->get_dec(1)));
 
-    delete a;
+        delete a;
+    }
+
+    /*
+    TEST(get_bin_test1)
+    {
+        std::string in = "ffffdebeaf";
+        squitter_short* a = new squitter_short(in);
+        CHECK_EQUAL(1099509448367,a->get_dec());
+
+        delete a;
+    }
+    }
+    */
 }
-
-TEST(get_bin_test_2)
-{
-    std::string in = "ff";
-    squitter* a = new squitter(in);
-    CHECK_EQUAL(255,(a->get_dec(0)*16 + a->get_dec(1)));
-
-    delete a;
-}
-
-TEST(get_bin_test_3)
-{
-    std::string in = "ff";
-    squitter_short* a = new squitter_short(in);
-    CHECK_EQUAL(255,(a->get_dec(0)*16 + a->get_dec(1)));
-
-    delete a;
-}
-
-/*
-TEST(get_bin_test1)
-{
-    std::string in = "ffffdebeaf";
-    squitter_short* a = new squitter_short(in);
-    CHECK_EQUAL(1099509448367,a->get_dec());
-
-    delete a;
-}
-*/
 }
